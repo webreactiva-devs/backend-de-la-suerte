501e39594140d9cdb67b963c3540b50f56dfe46a
Gabriel Martínez <gabriel@cysnet.es>
2022-03-17 18:35:31 +0100

    Commit del listado de emojis para el primer desafío

--- backend-de-la-suerte-app/src/App.js
+++ backend-de-la-suerte-app/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { createClient } from '@supabase/supabase-js'
+import { useState,useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [emojis, setEmojis] = useState([])
+  useEffect(() => {
+    getEmojis()
+  }, [])
+
+  const getEmojis = async () => {
+    try {
+      setLoading(true)
+
+      let { data, error, status } = await supabase
+        .from('emoji_db')
+        .select(`*`)
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setEmojis(data)
+      }
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
+    if(loading){
+      return (<div className='wrapper'><h1 className='loading-title'>Cargando...</h1></div>)
+    }
+    return (<ul className='listado-emojis'>
+      {
+        emojis.map((emoji) =>(
+            <li  className='emoji-item' key={emoji.id}><div className='emoji-icon'>{emoji.emoji}</div><div className='emoji-description'>{emoji.description}</div></li>
+          ))
+      }
+    </ul>)
 }
 
-export default App;
+
+
+
+export default App
